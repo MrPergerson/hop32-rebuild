d08f7bbf803e51a2a3a204bdeb63f167b8b450b8
Cole Pergerson <cole.pergerson@gmail.com>
2025-07-18 21:59:44 -0700

    jump experiment #1

--- players.lua
+++ players.lua
@@ -14,6 +14,15 @@ local maxPlayers = 32
 local maxFallVelocity = 100
 disabledPlayerCount = 0
 
+local jump_height = 64
+local jump_time_to_peak = .5
+local jump_time_to_fall = .5
+local jump_velocity = ((2 * ((jump_height/2))) / jump_time_to_peak) * -1
+local jump_gravity = ((-2 * ((jump_height/2)))  / (jump_time_to_peak * jump_time_to_peak)) * -1
+local fall_gravity = ((-2 * ((jump_height/2)))  / (jump_time_to_fall * jump_time_to_fall)) * -1
+
+local d_last_time = 0
+
 --voters = {}
 --votesToStart = 0
 
@@ -73,7 +82,7 @@ function initPlayers(startingCamPos_x, startingCamPos_y, dt)
                     vx = 0, 
                     vy = 0, 
                     onGround = false, 
-                    bounce_force = minBounceForce, 
+                    bounce_force = minBounceForce,
                     key=keyInput, 
                     sprite = sprite, 
                     disabled = false,
@@ -137,14 +146,16 @@ function update_players(game_pos_x, game_pos_y, dt)
             -- Apply grounded or ungrounded updates
             if player.onGround == false then
                 if player.vy > 0 then
-                    player.vy = min(player.vy + GRAVITY, maxFallVelocity)
+                    --player.vy += min(fall_gravity, maxFallVelocity)  * dt
+                    player.vy += fall_gravity * dt
                 else
-                    player.vy += GRAVITY
+                    player.vy += jump_gravity  * dt
                 end
             else
                 
                 player.bounce_force = max(player.bounce_force - bounceChargeRate, maxBounceForce)
                 if player.vy >= 0 then -- if just landed
+                    printh(time() - d_last_time)
                     player.vx = 0
                 end
             end
@@ -281,10 +292,12 @@ function bouncePlayer(key)
     local player = players[key]
     if not (player == nil) then
         if player.onGround and not(player.won) then
-            player.vy = player.bounce_force
-            player.vx = maxBounceRange
+            player.vy = jump_velocity --player.bounce_force
+            player.vx = 0 --maxBounceRange
             player.bounce_force = minBounceForce
             sfx(0)
+            d_last_time = time()
+            printh(time() .. " jump")
         end
         --[[
     else
